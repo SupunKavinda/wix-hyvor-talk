dd1d211c8818eba563ecb0fa7ff2d342d655cf09
SupunKavinda <44988673+SupunKavinda@users.noreply.github.com>
2021-06-03 11:30:15 +0530

    Update wix.js

--- wix.js
+++ wix.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+class HyvorTalkElement extends HTMLElement {
+  constructor() {
+    super();
+    console.log("Hyvor Talk Loading");
+  }
+
+  connectedCallback() {
+    
+  }
+}
+customElements.define('wix-hyvor-talk', HyvorTalkElement);
